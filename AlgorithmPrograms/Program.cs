a7d0d8f5b756a239f4c71148b1c39e7fd037de89
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-18 00:12:58 +0530

    feat: Welcome Message

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome To Algorithm Programs !");
+        }
+    }
+}
