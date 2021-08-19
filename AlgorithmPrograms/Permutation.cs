96e51996cb7f01c15d3bb0abe16d5491d3662d29
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-19 12:37:11 +0530

    feat: Added: All Permutations of a String

---
+++ AlgorithmPrograms/Permutation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class Permutation
+    {
+        public void Permute(String str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = Swap(str, l, i); //Fixing First Char
+                    Permute(str, l + 1, r); 
+                    str = Swap(str, l, i); 
+                }
+            }
+        }
+        public static String Swap(String a, int i, int j)
+        {
+            //Variable
+            char temp;
+
+            //Char Array
+            char[] charArray = a.ToCharArray();
+            
+            //Swapping Chars
+            temp = charArray[i];            
+            charArray[i] = charArray[j];            
+            charArray[j] = temp;            
+            string s = new string(charArray);            
+            return s; //return Value
+        }
+    }
+}
--- AlgorithmPrograms/UserChoice.cs
+++ AlgorithmPrograms/UserChoice.cs
@@ -11,10 +11,11 @@ namespace AlgorithmPrograms
             //Variable
             int userChoice;
 
-            //Display Message            
-            Console.WriteLine("Press - 1. Binary Search");
-            Console.WriteLine("Press - 2. Insertion Sort ");
-            Console.WriteLine("Press - 2. Bubble Sort ");
+            //Display Message
+            Console.WriteLine("Press - 1. Permutation of Strng");
+            Console.WriteLine("Press - 2. Binary Search");
+            Console.WriteLine("Press - 3. Insertion Sort ");
+            Console.WriteLine("Press - 4. Bubble Sort ");
             Console.WriteLine("Press - 0. Exit");
             Console.WriteLine("-----------------------------");
             Console.WriteLine("Enter your choice number : ");
@@ -24,19 +25,28 @@ namespace AlgorithmPrograms
             switch (userChoice)
             {
                 case 1:
+                    String str = "ABC";
+                    int n = str.Length;
+                    Console.WriteLine(":: Permutation Of String - {0} ::", str);
+                    Console.WriteLine("----------------------------------");
+                    Permutation permutation = new Permutation();                    
+                    permutation.Permute(str, 0, n - 1);
+                    break;
+
+                case 2:
                     Console.WriteLine(":: Binary Search To Find a Word ::");
                     Console.WriteLine("----------------------------------");
                     BinarySearch binarySearch = new BinarySearch();
                     binarySearch.inputArray();
                     break;
 
-                case 2:
+                case 3:
                     Console.WriteLine(":: Insertion Sort ::");
                     Console.WriteLine("----------------------------------");
                     InsertionSort insertionSort = new InsertionSort();
                     insertionSort.TakeFileData();
                     break;
-                case 3:
+                case 4:
                     Console.WriteLine(":: Bubble Sort ::");
                     Console.WriteLine("----------------------------------");
                     BubbleSort bubbleSort = new BubbleSort();
